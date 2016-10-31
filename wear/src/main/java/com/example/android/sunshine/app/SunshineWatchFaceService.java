5c36bc28e188ddc95512b8c4004cbb738bcb6f17
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-10-31 23:32:13 +0530

    basic watchface

---
+++ wear/src/main/java/com/example/android/sunshine/app/SunshineWatchFaceService.java
@@ -0,0 +1,107 @@
+package com.example.android.sunshine.app;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.view.SurfaceHolder;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class SunshineWatchFaceService extends CanvasWatchFaceService {
+
+    private static final long TICK_PERIOD_MILLIS = TimeUnit.SECONDS.toMillis(1);
+
+    @Override
+    public Engine onCreateEngine() {
+        return new SunshineEngine();
+    }
+
+    private class SunshineEngine extends CanvasWatchFaceService.Engine {
+
+        private Handler timeTick;
+        private SunshineWatchFace mSunshineWatchFaces;
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFaceService.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+                    .setAmbientPeekMode(WatchFaceStyle.AMBIENT_PEEK_MODE_HIDDEN)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .build());
+
+            timeTick = new Handler(Looper.myLooper());
+            startTimerIfNecessary();
+            mSunshineWatchFaces = SunshineWatchFace.newInstance(SunshineWatchFaceService.this);
+        }
+
+        private void startTimerIfNecessary() {
+            timeTick.removeCallbacks(timeRunnable);
+            if (isVisible() && !isInAmbientMode()) {
+                timeTick.post(timeRunnable);
+            }
+        }
+
+        private final Runnable timeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                onSecondTick();
+
+                if (isVisible() && !isInAmbientMode()) {
+                    timeTick.postDelayed(this, TICK_PERIOD_MILLIS);
+                }
+            }
+        };
+
+        private void onSecondTick() {
+            invalidateIfNecessary();
+        }
+
+        private void invalidateIfNecessary() {
+            if (isVisible() && !isInAmbientMode()) {
+                invalidate();
+            }
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            startTimerIfNecessary();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            mSunshineWatchFaces.setAntiAlias(!inAmbientMode);
+            mSunshineWatchFaces.setColor(inAmbientMode ? Color.GRAY : Color.WHITE);
+            mSunshineWatchFaces.setShowSeconds(!isInAmbientMode());
+            invalidate();
+            startTimerIfNecessary();
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onDestroy() {
+            timeTick.removeCallbacks(timeRunnable);
+            super.onDestroy();
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            super.onDraw(canvas, bounds);
+            mSunshineWatchFaces.draw(canvas, bounds);
+        }
+    }
+}
